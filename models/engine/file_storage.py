d523c30e8863b42a2701766e835dca6c7678e158
Abualiy <akremmuktar332@gmail.com>
2023-02-07 07:26:03 -0800

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,5 +27,40 @@ class FileStorage:
             json.dump(d, f)
 
     def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
+        classes = {"BaseModel": BaseModel,
+                "User": User,
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Place": Place,
+                "Review": Review}
+        return classes
+
+    def reload(self):
+         """Reloads the stored objects"""
+         if not os.path.isfile(FileStorage.__file_path):
+             return
+         with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+             obj_dict = json.load(f)
+             obj_dict = {k: self.classes()[v["__class__"]](**v)
+                     for k, v in obj_dict.items()}
+             FileStorage.__objects = obj_dict
+
+    def attributes(self):
+         """Returns the valid attributes and their types for classname"""
+         attributes = {
+                 "BaseModel":
+                    {"id": str,
+                     "created_at": datetime.datetime,
+                     "updated_at": datetime.datetime},
+                "User":
 
