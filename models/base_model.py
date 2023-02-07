22b10e937c3063a80e3cf1251bf73f50b3fb802a
Abualiy <87162921+Abualiy@users.noreply.github.com>
2023-02-07 20:52:31 +0300

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""this is the base model"""
+"""This script is the base model"""
 
 import uuid
-from datetime import daretime
+from datetime import datetime
 from models import storage
 
 
 class BaseModel:
 
-    """ Class from which all other class will inherit"""
+    """Class from which all other classes will inherit"""
 
     def __init__(self, *args, **kwargs):
         """Initializes instance attributes
@@ -21,4 +21,36 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["creates_at"] = d
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """Returns official string representation"""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
